2f3f8036779bbf26b35bc163c8aa5f3a8b5964cb
Ayub Ian <iayub.mia@gmail.com>
2023-03-26 10:55:07 +0300

    Added model validations

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,7 @@
 class HeroPower < ApplicationRecord
   belongs_to :hero
   belongs_to :power
+
+  validates :strength, inclusion: { in: %w(strong weak average),
+    message: "%{value} is not a valid strength" }
 end
