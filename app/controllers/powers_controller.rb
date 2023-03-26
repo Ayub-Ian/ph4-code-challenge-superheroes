0016cdcfa96992dcf300d3c51a2cb87e15b82af2
Ayub Ian <iayub.mia@gmail.com>
2023-03-26 11:31:42 +0300

    todo: get power using params

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -3,4 +3,16 @@ class PowersController < ApplicationController
     def index
         render json: Power.all, status: :ok
     end
+
+    # GET /powers/:id
+    def show
+        power = Power.find_by(id: params[:id])
+        if power
+            render json: power, status: :ok
+        else
+            render json: {
+                error: "Power not found"
+              }, status: :not_found
+        end
+    end
 end
