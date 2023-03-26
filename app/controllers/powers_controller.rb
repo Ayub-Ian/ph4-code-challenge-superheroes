fc2691a98c1824d3a99ce2275a0f85a9a0b5b28a
Ayub Ian <iayub.mia@gmail.com>
2023-03-26 11:41:05 +0300

    todo: update power

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -15,4 +15,23 @@ class PowersController < ApplicationController
               }, status: :not_found
         end
     end
+
+    # PUT /powers/:id
+    def update
+        power = Power.find_by(id: params[:id])
+        if power
+            power.update(power_params)
+            render json: power, status: :ok
+        else
+            render json: {
+                error: "Power not found"
+              }, status: :not_found
+        end
+    end
+
+    private 
+    
+    def power_params
+        params.permit(:name, :description)
+    end
 end
